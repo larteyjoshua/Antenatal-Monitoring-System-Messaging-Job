ba6ee80a8ba83f34f882904bbbfb09f2f9ab05fd
Lartey Joshua <larteyjoshua@gmail.com>
2023-06-16 02:14:51 +0000

    Switch to MYSQL

--- app/models/model.py
+++ app/models/model.py
@@ -7,10 +7,10 @@ from app.utils.database import Base
 class Admin(Base):
     __tablename__ = 'admins'
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    email = Column(String)
-    password = Column(String)
-    telephone = Column(String)
+    name = Column(String(655))
+    email = Column(String(655))
+    password = Column(Text)
+    telephone = Column(String(655))
     isActive = Column(Boolean(), default=True)
     dateAdded = Column(DateTime, default=datetime.now)
     mother = relationship("ExpectedMother", back_populates="admins")
@@ -20,17 +20,17 @@ class Admin(Base):
 class ExpectedMother(Base):
     __tablename__ = 'ExpectedMothers'
     id = Column(Integer, primary_key=True, index=True)
-    surname = Column(String)
-    first_name = Column(String)
-    telephone = Column(String)
+    surname = Column(String(655))
+    first_name = Column(String(655))
+    telephone = Column(String(655))
     dateAdded = Column(DateTime, default=datetime.now)
     weight = Column(Float, nullable=True)
     height = Column(Float, nullable=True)
-    birth_date = Column(String, nullable=True)
-    location = Column(String, nullable=True)
-    first_antenatal_visit_date = Column(String, nullable=True)
-    expected_delivery_date = Column(String, nullable=True)
-    login_pin = Column(String)
+    birth_date = Column(String(655), nullable=True)
+    location = Column(String(655), nullable=True)
+    first_antenatal_visit_date = Column(String(655), nullable=True)
+    expected_delivery_date = Column(String(655), nullable=True)
+    login_pin = Column(Text)
 
     added_by = Column(Integer, ForeignKey('admins.id'))
 
@@ -44,12 +44,12 @@ class Appointment(Base):
     __tablename__ = 'appointments'
     id = Column(Integer, primary_key=True, index=True)
     expected_mother_id = Column(Integer, ForeignKey('ExpectedMothers.id'))
-    appointed_date = Column(String)
-    appointed_time = Column(String)
+    appointed_date = Column(String(655))
+    appointed_time = Column(String(655))
     attended = Column(Boolean(), default=False)
     appointed_set_by = Column(Integer, ForeignKey('admins.id'))
     dateAdded = Column(DateTime, default=datetime.now)
-    appointment_note = Column(String)
+    appointment_note = Column(Text)
 
     admin = relationship("Admin", back_populates="appointment")
     expected_mother = relationship(
@@ -61,7 +61,7 @@ class Appointment(Base):
 class Comment(Base):
     __tablename__ = 'comments'
     id = Column(Integer, primary_key=True, index=True)
-    message = Column(String)
+    message = Column(Text)
     expected_mother_id = Column(Integer, ForeignKey('ExpectedMothers.id'))
     dateAdded = Column(DateTime, default=datetime.now)
     appointment_id = Column(Integer, ForeignKey('appointments.id'))
@@ -73,7 +73,7 @@ class Comment(Base):
 class Voice(Base):
     __tablename__ = 'voices'
     id = Column(Integer, primary_key=True, index=True)
-    recording_name = Column(String)
+    recording_name = Column(Text)
     expected_mother_id = Column(Integer, ForeignKey('ExpectedMothers.id'))
     dateAdded = Column(DateTime, default=datetime.now)
     appointment_id = Column(Integer, ForeignKey('appointments.id'))
